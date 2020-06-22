0c0dfe82eb3ca824e26ccf503b2099c8a9cc1b5f
itay hubara <itayhubara@users.noreply.github.com>
2020-06-22 13:06:36 +0300

    Update create_calib_data.py

--- BERT-base/scripts/create_calib_data.py
+++ BERT-base/scripts/create_calib_data.py
@@ -4,7 +4,6 @@ with open (train_file_path, 'rt') as myfile:  # Open file lorem.txt for reading 
     for txt_data in myfile:                 # For each line, read it to a string 
         id=txt_data.find('"title"',101385+12)
         new_line= txt_data[:id-3]+txt_data[-20:]
-        import pdb; pdb.set_trace()
 calib_path= train_file_path.replace('train','calib')        
 calib_file = open(calib_path, "wt")
 n = calib_file.write(new_line)
